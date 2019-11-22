85d88156b8f46f020ae166b532121ca039f52524
Jampanisatishbabu <38609274+Jampanisatishbabu@users.noreply.github.com>
2019-11-22 11:06:00 +0530

    Update P1.py

--- P1.py
+++ P1.py
@@ -1,8 +1,8 @@
 # This program adds two numbers provided by the user
  
 # Store input numbers
-num1 = input('Enter first number: ')
-num2 = input('Enter second number: ')
+num1 = 5
+num2 = 6
  
 # Add two numbers
 sum = float(num1) + float(num2)
